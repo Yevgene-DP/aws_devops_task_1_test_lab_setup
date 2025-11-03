dbe230707d870b07cd7ec74ee8db3ecd23e51666
Yevgene-DP <ebalalay@gmail.com>
2025-11-03 20:14:51 +0200

    Complete AWS DevOps Task 1: VPC deployment

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
-# Uncomment the resource bellow to complete the task: 
-# resource "aws_vpc" "network" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     Name = "mate-aws-grafana-lab"
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "main-vpc"
+  }
+}
